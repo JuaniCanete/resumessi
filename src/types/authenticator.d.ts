fd21b8bfe63d44585d61767f98028275e1528c1b
Juani Cañete <48571668+JuaniCanete@users.noreply.github.com>
2026-08-06 23:56:04 -0300

    chore: removing authenticator as codegent with storage state is the only way we'll support

--- src/types/authenticator.d.ts
+++
@@ -1,5 +0,0 @@
-declare module 'authenticator' {
-  export function generateToken(formattedKey: string): string;
-  export function generateKey(): string;
-  export function generateURI(formattedKey: string, accountName: string, issuer: string): string;
-}
